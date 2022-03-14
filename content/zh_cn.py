dcdfa582fc43f974c2173046175d7bd27299b822
ChosenQ <chosenqiuch@gmail.com>
2022-03-14 17:31:24 +0800

    commit

--- content/zh_cn.py
+++ content/zh_cn.py
@@ -1,5 +1,5 @@
 content = {
-    "name": '''Chosen(Chen) Qiu <div style="height: 260px; width: 170px;"><img alt="" src="./assets/profile.jpg" style="height: auto; max-width: 100%;" /></div>''',
+    "name": '''Chosen(Chen) Qiu <div style="height: 260px; width: 170px;float:right"><img alt="" src="./assets/profile.jpg" style="height: auto; max-width: 100%;" /></div>''',
     "title": "Chosen's Resume",
 
     "extra_style": "zh_cn.css",
